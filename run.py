8ba81a14b46ba20248a2cc710873f339ff3d1983
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-06-26 02:25:16 +0100

    Add waitress for serving in production

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import os
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
     host = os.environ.get('HOST') or '0.0.0.0'
     port = os.environ.get('PORT') or 5000
-    app.run(host=host, port=port, debug=True)
+
+    if os.environ.get("FLASK_ENV", "development") == "development":
+        app.run(host=host, port=port, debug=True)
+    else:
+        serve(app, host=host, port=port)
