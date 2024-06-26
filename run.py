e06ea5af8ee6b42f655a3dbd1655ea62221b939f
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-06-26 02:28:24 +0100

    Change conditions for serving in production :)

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     host = os.environ.get('HOST') or '0.0.0.0'
     port = os.environ.get('PORT') or 5000
 
-    if os.environ.get("FLASK_ENV", "development") == "development":
-        app.run(host=host, port=port, debug=True)
-    else:
+    if os.environ.get("FLASK_ENV", "development") == "production":
         serve(app, host=host, port=port)
+    else:
+        app.run(host=host, port=port, debug=True)
